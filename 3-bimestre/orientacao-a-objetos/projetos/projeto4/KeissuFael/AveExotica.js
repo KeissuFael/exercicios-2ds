becdf4603f6ab287bc0160dd0cb811b6ecdf68cb
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-09-22 21:06:49 -0300

    Delete 3-bimestre/orientacao-a-objetos/projetos/projeto4/KeissuFael/AveExotica.js

--- 3-bimestre/orientacao-a-objetos/projetos/projeto4/KeissuFael/AveExotica.js
+++
@@ -1,9 +0,0 @@
-const Paciente = require("./Paciente")
-
-class AveExotica extends Paciente {
-    descricao() {
-        return `Consulta de ave exótica registrada`
-    }
-}
-
-module.exports = AveExotica
