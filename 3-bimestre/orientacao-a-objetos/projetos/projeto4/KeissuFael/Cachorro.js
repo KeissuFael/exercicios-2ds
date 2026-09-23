279a364312e007c576966d5ae052a9f5c8956b0d
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2026-09-22 21:06:56 -0300

    Delete 3-bimestre/orientacao-a-objetos/projetos/projeto4/KeissuFael/Cachorro.js

--- 3-bimestre/orientacao-a-objetos/projetos/projeto4/KeissuFael/Cachorro.js
+++
@@ -1,9 +0,0 @@
-const Paciente = require("./Paciente")
-
-class Cachorro extends Paciente {
-    descricao() {
-        return `Consulta de cachorro registrada`
-    }
-}
-
-module.exports = Cachorro
